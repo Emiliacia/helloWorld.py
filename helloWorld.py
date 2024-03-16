e78a97194b3be2bc9621949d3c90c0e37f8aee8d
Emilia Bataniuc <bataniucemilia@gmail.com>
2024-03-16 16:53:02 +0000

    Implemented new_method for specific functionality

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,14 @@
 #Asking the user to write something 
 user_input = input("Please enter something: ")
 print("You entered:", user_input)
+
+def new_method():
+    print("This is the new method!")
+    user_input = input("Please enter something: ")
+    print("You entered:", user_input)
+    print("Number of characters:", len(user_input))
+user_input = input("Please enter something: ")
+print("You entered:", user_input)
+
+new_method()
+
